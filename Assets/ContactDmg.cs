e370d870e7438d25bd6e0a4ce32b65970193e5c6
Valwiler <tymotheilanouette@gmail.com>
2021-03-02 15:42:56 -0500

    Gossage du Caliss

--- Assets/ContactDmg.cs
+++ Assets/ContactDmg.cs
@@ -4,9 +4,22 @@ using UnityEngine;
 
 public class ContactDmg : MonoBehaviour
 {
+    private BoxCollider2D OGBounds;
+
+    private BoxCollider2D FloorZone;
+
+    private BoxCollider2D ContactZone;
     // Start is called before the first frame update
     void Start()
     {
+        OGBounds = GetComponent<BoxCollider2D>();
+        FloorZone = new BoxCollider2D();
+        ContactZone = new BoxCollider2D();
+        
+        FloorZone.size = OGBounds.size;
+        FloorZone.
+        ContactZone.size = OGBounds.size;
+        ContactZone.isTrigger = true;
         
     }
 
